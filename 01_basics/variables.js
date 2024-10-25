c0859d82486baa2e3221863305d565517b671748
Garvit Gandhi <138232732+GARVIT256@users.noreply.github.com>
2024-10-25 08:46:59 +0000

    datatypes

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,6 +1,6 @@
 const accountId = 1254
 let accountEmail = "gg@google.com"
 var accountPass = "1234"
-// never use var
+// never use var bcoz issue of block scope 
 // const cannot be changed
 console.table([accountId,accountEmail,accountPass])
