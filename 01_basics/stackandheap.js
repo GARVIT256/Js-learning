9749bb79e03402cf6a6e4912bff7935db1045591
Garvit Gandhi <138232732+GARVIT256@users.noreply.github.com>
2024-10-27 07:02:07 +0000

    stack and heap

---
+++ 01_basics/stackandheap.js
@@ -0,0 +1,14 @@
+let myName = "Garvit"
+let myNewName = myName
+myNewName = "Garvit Gandhi"
+console.log(myName);
+console.log(myNewName);
+let userOne ={
+    email: "garvit@google.com",
+    accId: 2005
+}
+let userTwo = userOne
+userTwo.email="garvitgandi@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
